27c410a54e2cccba62427cf51c5123c036b598a4
marlon ureta <fabrisio021@gmail.com>
2025-02-27 20:05:21 -0500

    added gestion producto

--- app/src/main/java/com/example/proyectofinal/BaseActivity.kt
+++ app/src/main/java/com/example/proyectofinal/BaseActivity.kt
@@ -4,7 +4,6 @@ import android.app.AlertDialog
 import android.content.Intent
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 open class BaseActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
--- app/src/main/java/com/example/proyectofinal/GestionProducto.kt
+++ app/src/main/java/com/example/proyectofinal/GestionProducto.kt
@@ -1,28 +1,20 @@
 package com.example.proyectofinal
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : BaseActivity() {
+class GestionProducto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_app)
+        setContentView(R.layout.activity_gestion_producto)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val backButton = findViewById<Button>(R.id.button_regresar_main)
-        backButton?.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
     }
 }
